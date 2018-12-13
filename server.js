757df731f66506a0a70a9e8ae9c9b36e8bd8a69d
jmj928 <jmj928@vt.edu>
2018-12-13 18:48:08 -0500

    fixing stupid mongo

--- server.js
+++ server.js
@@ -24,10 +24,16 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var mongoURL = process.env.MONGODB_URI || "mongodb://localhost:27017/mongoHeadlines";
 
 // mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-mongoose.connect(MONGODB_URI);
+mongoose.connect(mongoURL);
+
+// var nodeEnv = process.env.NODE_ENV || "development";
+
+// let mongoURL; 
+// mongoURL = process.env.MONGODB_URI; 
+// mongoose.connect(mongoURL);
 
 app.get("/scrape", function(req, res) {
 
