053463f50ca7c040371bc31dc06c7b167ad63fa1
jmj928 <jmj928@vt.edu>
2018-12-15 10:00:35 -0500

    adding original files back

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
